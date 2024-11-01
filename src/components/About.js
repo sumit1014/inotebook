2367bc01b733e6104ba19133b9a82a8f4da5fed1
sumit soni <142312609+sumit1014@users.noreply.github.com>
2024-11-01 19:06:06 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from 'react'
 const About = () => {
     return(
-    <div>
-      <h3>About</h3>
+    <div className="container d-flex justify-content-center">
+      <div className="card my-4 w-75">
+        <div className="card-header">
+        About iNotebook
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">i-notebook</h5>
+          <p className="card-text">Welcome to iNotebook! This application, built using ReactJS, is designed to be your digital companion for organizing thoughts, notes, and ideas. With a focus on user-friendly interfaces and efficient functionality, iNotebook simplifies note-taking, making it easy to capture and organize information seamlessly. Enjoy a smooth and responsive experience as you create, edit, and manage your notes all in one place.</p>
+        </div>
+      </div>
     </div>
     )
 }
